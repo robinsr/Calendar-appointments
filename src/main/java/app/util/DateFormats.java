ddd58d974ea6893afe33c6a462fb3a218a781eb7
Ryan Robinson <ryan.b.robinson@gmail.com>
2018-04-29 16:34:48 -0700

    Updated date formats to use format that is expected by the frontend

--- src/main/java/app/util/DateFormats.java
+++ src/main/java/app/util/DateFormats.java
@@ -1,7 +1,7 @@
 package app.util;
 
 public class DateFormats {
-    public static String DATETIME = "MM/dd/YYYY h:mm:ss aaa";
-    public static String DATE = "MM/dd/YYYY";
+    public static String DATETIME = "M/d/YYYY h:mm:ss aaa";
+    public static String DATE = "M/d/YYYY";
     public static String TIME = "h:mm:ss aaa";
 }
